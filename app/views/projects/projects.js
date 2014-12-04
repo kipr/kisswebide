56d43e455e7f46c8a48b9d3134b10f936bdfb905
Stefan Zeltner <zeltner@usc.edu>
2014-12-03 19:20:43 -0600

    Initial commit

---
+++ app/views/projects/projects.js
@@ -0,0 +1,18 @@
+"use strict";
+
+angular.module('kissWebIdeControllers')
+.controller('ProjectsController', ['$scope', '$location', 'target',
+    function ($scope, $location, target) {
+        $scope.target = target;
+        
+        var editor = ace.edit( 'file_content' );
+        editor.setTheme( 'ace/theme/crimson_editor' );
+        editor.getSession().setMode( 'ace/mode/c_cpp' );
+        
+        $scope.$watch('target.fileResource', function(newValue, oldValue) {
+            if(newValue) {
+                editor.setValue(Base64.decode(target.fileResource.content), -1);
+            }
+        });
+    }
+]);
