c0a60367279ae16dafd4c2f1e907ca3d31c45ca7
Stefan Zeltner <zeltner@usc.edu>
2014-12-03 21:47:52 -0600

    Add content to project(s) view

--- app/views/project/project.js
+++ app/views/project/project.js
@@ -1,8 +1,16 @@
 "use strict";
 
 angular.module('kissWebIdeControllers')
-.controller('ProjectController', ['$scope', '$location',
-    function ($scope, $location) {
-        
+.controller('ProjectController', ['$scope', 'target',
+    function ($scope, target) {
+        $scope.target = target;
+                
+        $scope.selectItem = function(fileName) {
+            if(fileName == $scope.target.fileName) {
+                $scope.target.fileName = undefined;
+            } else {
+                $scope.target.fileName = fileName;
+            }
+        }
     }
 ]);
--- app/views/projects/projects.js
+++ app/views/projects/projects.js
@@ -1,7 +1,16 @@
 "use strict";
 
 angular.module('kissWebIdeControllers')
-.controller('ProjectsController', ['$scope', '$location',
-    function ($scope, $location) {
+.controller('ProjectsController', ['$scope', 'target',
+    function ($scope, target) {
+        $scope.target = target;
+                
+        $scope.selectItem = function(projectName) {
+            if(projectName == $scope.target.projectName) {
+                $scope.target.projectName = undefined;
+            } else {
+                $scope.target.projectName = projectName;
+            }
+        }
     }
 ]);
