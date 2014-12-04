9f3ec2fd38dc3c2fc4bb4364d09e581708eefa9b
Stefan Zeltner <zeltner@usc.edu>
2014-12-03 19:19:03 -0600

    Initial commit

---
+++ app/views/target/target.js
@@ -0,0 +1,7 @@
+"use strict";
+
+angular.module('kissWebIdeControllers')
+.controller('TargetController', ['$scope',
+    function ($scope) {
+    }
+]);
---
+++ app/views/user/user.js
@@ -0,0 +1,7 @@
+"use strict";
+
+angular.module('kissWebIdeControllers')
+.controller('UserController', ['$scope',
+    function ($scope) {
+    }
+]);
