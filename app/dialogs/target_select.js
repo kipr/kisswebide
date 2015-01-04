8318eef92a38c780321b8a6d91cba8aedf5dd3ec
Stefan Zeltner <zeltner@usc.edu>
2015-01-04 12:04:13 +0100

    Improve the target select dialog

--- app/dialogs/target_select.js
+++ app/dialogs/target_select.js
@@ -5,16 +5,31 @@ angular.module('kissWebIdeControllers')
 .controller('TargetSelectDialogController', ['$scope', '$modalInstance', 'target',
     function ($scope, $modalInstance, target) {
         $scope.target = target;
-        $scope.target.username = 'User';
-        $scope.targetPassword = 'KISS';
-        $scope.targetIp = '192.168.123.192';
+        
+        $scope.targetUsername = target.username || '';
+        $scope.targetPassword = '';
+        $scope.targetIp = '';
         $scope.targetLocation = 'target_is_server';
         
+        if(target.name) {
+            var matches = target.name.match(/^http:\/\/(.*)\/api/);
+            if(matches[1]) {
+                if(matches[1] == window.location.hostname) {
+                    $scope.targetLocation = 'target_is_server';
+                } else {
+                    $scope.targetLocation = 'target_by_ip';
+                    $scope.targetIp = matches[1];
+                }
+            }
+        }
+        
         $scope.logIn = function() {
-            if(!$scope.target.username) {
+            if(!$scope.targetUsername) {
                 alert('Please enter a valid username');
                 return;
             }
+            
+            target.username = $scope.targetUsername;
             
             var url;
             if($scope.targetLocation === 'target_is_server') {
